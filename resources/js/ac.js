03f16e3a76753f09e21d550bd14637728ff6c87b
anhpi1 <phamhaianh660@gmail.com>
2024-10-14 19:00:57 +0700

    Update ac.js

--- resources/js/ac.js
+++ resources/js/ac.js
@@ -17,6 +17,6 @@ function quen_mat_khau_success() {
     window.location.href = "quen_mat_khau_success.html";
 }
 function logg() {
-    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0";
 }
 
