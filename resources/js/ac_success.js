511e87c40c1ea3c208c2fa20f1ff15a2370a5ccd
anhpi1 <phamhaianh660@gmail.com>
2024-10-14 14:30:05 +0700

    hai anh

---
+++ resources/js/ac_success.js
@@ -0,0 +1,8 @@
+document.getElementById('button_dang_nhap_success').addEventListener('click', () => {
+    dang_ki_success();
+});
+
+
+function dang_ki_success() {
+    window.location.href = "dang_ki_success.html";
+}
