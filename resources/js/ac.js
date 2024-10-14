0856f2cf389e556cc4e43e3982823cd9ff986e4f
anhpi1 <phamhaianh660@gmail.com>
2024-10-14 14:52:14 +0700

    haianh

--- resources/js/ac.js
+++ resources/js/ac.js
@@ -1,14 +1,16 @@
-document.getElementById('button_dang_nhap_start').addEventListener('click', () => {
-    dang_nhap();
-});
-document.getElementById('button_dang_ki_start').addEventListener('click', () => {
-    dang_ki();
-});
+
 
 function dang_nhap() {
     window.location.href = "dang_nhap.html";
 }
 function dang_ki() {
     window.location.href = "dang_ki.html";
+}
+
+function dang_ki_success() {
+    window.location.href = "dang_ki_success.html";
+}
+function dang_nhap_success() {
+    window.location.href = "dang_nhap_success.html";
 }
 
--- resources/js/ac_success.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('button_dang_nhap_success').addEventListener('click', () => {
-    dang_ki_success();
-});
-
-
-function dang_ki_success() {
-    window.location.href = "dang_ki_success.html";
-}
