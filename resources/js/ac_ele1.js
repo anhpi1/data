5fd85127fb09b8c2694fa6f79b40722a01315adb
anhpi1 <phamhaianh660@gmail.com>
2024-10-25 21:41:39 +0700

    haianh

--- resources/js/ac_ele1.js
+++
@@ -1,7 +0,0 @@
-function checkUncheck(checkbox) {
-    if (checkbox.checked) {
-        div1.classList.add("move"); 
-    } else {
-        div1.classList.remove("move"); 
-    }
-}
--- resources/js/ac_menu2.js
+++
@@ -1,6 +0,0 @@
-function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('checkboxes');
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false;
-    });
-}
